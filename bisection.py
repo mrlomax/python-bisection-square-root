da3eb5660b0d944c5776264d8cdbeda8a7aa2d1a
mrlomax <mattylomax@googlemail.com>
2025-09-23 17:40:44 +0100

    WIP: Created initial square_root_bisection function

--- bisection.py
+++ bisection.py
@@ -1,3 +1,12 @@
+def square_root_bisection(square_target, tolerance= 1e-7, max_iterations= 100):
+    if square_target < 0:
+        raise ValueError("Square root of negative number is not defined in real numbers")
+    elif square_target == 1:
+        root = 1
+        print(f'The square root of {square_target} is 1')
+    elif square_target == 0:
+        pass
+
 def main ():
     pass
 
